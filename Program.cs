54720e53146465bd1db946bd67fec1a06e184297
Mert Yılmaz <nexom8bit@gmail.com>
2025-09-01 21:56:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Hayvan3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kus serce = new Kus();
+            serce.Ad = "Serçe";
+            serce.tur = "kuş";
+            serce.yas = 2;
+            serce.yasamalanı = "Ağaçlar";            
+                
+          serce.bilgiyaz();
+
+
+            Kaplan kaplan = new Kaplan();
+            kaplan.Ad = "Kara";
+            kaplan.tur = "BENGAİ";
+            kaplan.yas = 12;
+            kaplan.yasamalanı = "ORMAN";
+
+            kaplan.bilgiyaz();
+
+
+
+
+        }
+        public abstract class Hayvan
+
+        {
+            public  string Ad {  get; set; }
+            public  string tur {  get; set; }
+
+            public int yas { get; set; }
+
+            public string yasamalanı { get; set; }
+
+
+
+            public abstract void bilgiyaz();
+
+        }
+        public class Kus : Hayvan
+        {
+          
+            
+           public override void bilgiyaz()
+            {
+
+                Console.WriteLine($"Kuş Ad: {Ad}, Tür: {tur}, Yaş: {yas}, Yaşam Alanı: {yasamalanı}");
+
+            }
+               
+        }
+        public class Kaplan : Hayvan
+        {
+            public override void bilgiyaz()
+            {
+                Console.WriteLine($"Kaplan Ad: {Ad}, Tür: {tur}, Yaş: {yas}, Yaşam Alanı: {yasamalanı}");
+            }
+        }
+    }
+   
+}
